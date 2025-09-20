dbb25fc7bb8833b103a63142f9051139c1e373de
vansh-frontend <vanshdhalor04@gmail.com>
2025-09-20 10:37:35 +0530

    changes

--- components/firebase/firebaseConfig.ts
+++ components/firebase/firebaseConfig.ts
@@ -1,4 +1,5 @@
 // components/firebase/firebaseConfig.ts
+'use client';
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
